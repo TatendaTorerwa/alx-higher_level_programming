7bd97436f4f532cd579a790ab8eefba010eff5e6
TatendaTorerwa <ttorerwa01@gmail.com>
2023-10-23 04:40:38 -0700

    Pthon bytecode.

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,12 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/pyuthon3
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 3):
+
+    for i in range(1, 4):
         try:
             if i > a:
-                return Exception('Too far')
-        except:
+                raise Exception('Too far')
+            result += (a ** b) / i
+        except Exception as e:
             pass
-        result = result + (a ** b / i)
+
     result += a + b
+
     return result

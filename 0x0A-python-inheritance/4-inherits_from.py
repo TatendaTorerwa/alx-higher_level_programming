86b628ac10dd8eb108881620f4440e9e110b400b
TatendaTorerwa <ttorerwa01@gmail.com>
2023-11-06 03:27:23 -0800

    4. Only sub class of.

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines an inherited class checking function."""
+
+
+def inherits_from(obj, a_class):
+    """Checks ifan object is a subclass.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The type of class.
+
+    Returns:
+        True - If the object is a subclass
+        Otherwise - False.
+    """
+
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        False

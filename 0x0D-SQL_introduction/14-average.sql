9b079d29d73e30cd19047e40e7f4d848ba72a12e
TatendaTorerwa <ttorerwa01@gmail.com>
2023-12-12 13:30:16 -0800

    14. Average .

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Computes the score average of all records in the table second_table. 
 -- Of the database hbtn_0c_0 in your MySQL server.
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score) AS average FROM second_table;

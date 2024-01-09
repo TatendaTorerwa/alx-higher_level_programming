578cb4605635f8cc49744ed5dc0eeac398200d8f
TatendaTorerwa <ttorerwa01@gmail.com>
2024-01-08 22:46:13 -0800

    8. Square.

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
-}else {
-    for (let i = 0; i < Number(process.argv[2]); i++) {
-      let row = '';
-      for (let j = 0; j < Number(process.argv[2]); j++) {
-        row += 'X';
-      }
-      console.log(row);
+} else {
+  for (let i = 0; i < Number(process.argv[2]); i++) {
+    let row = '';
+    for (let j = 0; j < Number(process.argv[2]); j++) {
+      row += 'X';
     }
+    console.log(row);
+  }
 }
-

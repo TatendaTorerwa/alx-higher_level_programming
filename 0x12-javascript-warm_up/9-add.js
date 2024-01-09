b2b7222bdb0a818dfafd8093a1225507af4750d5
TatendaTorerwa <ttorerwa01@gmail.com>
2024-01-08 16:50:32 -0800

    9. Add.

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-function add(a, b) {
-  let sum = a + b;
+function add (a, b) {
+  const sum = a + b;
   return console.log(sum);
 }
-add (Number(process.argv[2]), Number(process.argv[3]));
-
+add(Number(process.argv[2]), Number(process.argv[3]));

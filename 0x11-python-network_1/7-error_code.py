b45bd53de3fe9537570763d0a2de32728734f696
TatendaTorerwa <ttorerwa01@gmail.com>
2024-01-26 12:58:19 -0800

    7. Error code #1.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,5 +12,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     res = requests.get(url)
-    if ( res.status_code >= 400):
+    if (res.status_code >= 400):
         print("Error code: {}".format(res.status_code))
